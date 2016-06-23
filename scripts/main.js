d7334cc44ecdafdde5ed5962b016408a020dc9bc
Brian Rippeto <rippeto@gmail.com>
2016-06-23 12:53:37 -0600

    React routes established for app and storepicker

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,17 @@
 var React = require('react');
 var ReactDom = require('react-dom');
 
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Navigation = ReactRouter.Navigation;
+var History = ReactRouter.History;
+
+var createBrowserHistory = require('history/lib/createBrowserHistory');
+
+var h = require('./helpers');
+
+
 var App = React.createClass({
   render : function() {
     return (
@@ -46,18 +57,39 @@ var Inventory = React.createClass({
 })
 
 var StorePicker = React.createClass({
-
+  mixins : [History],
+  goToStore : function(event) {
+    event.preventDefault();
+    var storeId = this.refs.storeId.value;
+    this.history.pushState(null, '/store/' + storeId)
+  }
   render : function() {
-    var name = "brian";
     return (
-      <form className="store-selector">
-        {/* Comment syntax */}
-        <h2>Please Enter a Store {name}</h2>
-        <input type="text"ref="storeId" required />
+      <form className="store-selector" onSubmit={this.goToStore}>
+        <h2>Please Enter a Store</h2>
+        <input type="text"ref="storeId" defaultValue={h.getFunName()} required />
         <input type="submit" />
       </form>
     )
   }
 });
 
-ReactDom.render(<App/>, document.querySelector('#main'));
+var NotFound = React.createClass({
+  render : function() {
+    return <h1>Not Found!</h1>
+  }
+})
+
+/*
+  Routes
+*/
+
+var routes = (
+  <Router history={createBrowserHistory()}>
+    <Route path="/" component={StorePicker}/>
+    <Route path="/store/:storeId" component={App}/>
+    <Route path="*" component={NotFound}/>
+  </Router>
+)
+
+ReactDom.render(routes, document.querySelector('#main'));
