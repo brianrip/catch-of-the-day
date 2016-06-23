0a4b624ce402289aaa73663515b4e0ce0d3fd5aa
Brian Rippeto <rippeto@gmail.com>
2016-06-23 12:16:39 -0600

    render app components

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,63 @@
 var React = require('react');
 var ReactDom = require('react-dom');
 
+var App = React.createClass({
+  render : function() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market" />
+        </div>
+        <Order />
+        <Inventory />
+      </div>
+    )
+  }
+})
+
+var Header = React.createClass({
+  render : function() {
+    return (
+      <header className="top">
+        <h1>Catch
+        <span className="ofThe">
+          <span className="of">of</span>
+          <span className="the">the</span>
+        </span>
+        day</h1>
+        <h3 className="tagline"><span>{this.props.tagline}</span></h3>
+      </header>
+    )
+  }
+})
+var Order = React.createClass({
+  render : function() {
+    return (
+      <p>Order</p>
+    )
+  }
+})
+var Inventory = React.createClass({
+  render : function() {
+    return (
+      <p>Inventory</p>
+    )
+  }
+})
+
 var StorePicker = React.createClass({
 
   render : function() {
+    var name = "brian";
     return (
-      <p>Hello!</p>
+      <form className="store-selector">
+        {/* Comment syntax */}
+        <h2>Please Enter a Store {name}</h2>
+        <input type="text"ref="storeId" required />
+        <input type="submit" />
+      </form>
     )
   }
 });
 
-ReactDom.render(<StorePicker/>, document.querySelector('#main'))
+ReactDom.render(<App/>, document.querySelector('#main'));
